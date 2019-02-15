c08fcf44c7a2e8fb78909049abaa62a3c8b3deeb
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-02-15 20:44:45 -0300

    Add files via upload
    matriz error-hat que faz parte do omega o qual muda o hc de acordo com o method escolhido

--- codigo_pibic_parte2.cpp
+++ codigo_pibic_parte2.cpp
@@ -5,17 +5,29 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-List mats(arma::mat X) {
+arma::mat mats(arma::mat X, List model, int method) {
   
   arma::mat V = X.t() * X;
   arma::mat bread1 = inv(V) * X.t(); 
   arma::mat bread2 = X * V.i();
-  return  List::create(_["bread_1"] = bread1,
-                       _["bread_2"] = bread2);
+  
+  // criando a matriz error_hat
+  NumericVector residuals =  model["residuals"];
+  int dim = residuals.size();
+  arma::mat error_hat = arma::zeros(dim,dim);
+  
+  for (int i = 0; i < dim; i++){
+    error_hat(i,i) = residuals[i];
+  }
+  
+  // List::create(_["bread_1"] = bread1,
+     //          _["bread_2"] = bread2)
+  
+  return error_hat;
   
 }
 
 
  /*** R
-  mats(M)
- */
+  mats(X = M, model = modelo, method = 0)
+*/
