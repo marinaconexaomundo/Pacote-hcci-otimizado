7b5ebfbe421b0d43aef7b54d9a1aca3bf1db2036
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-02-16 10:25:56 -0300

    mathod = 0 e = 2 funcionando.

--- codigo_pibic_parte2.cpp
+++ codigo_pibic_parte2.cpp
@@ -5,13 +5,18 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-arma::mat mats(arma::mat X, List model, int method) {
+arma::mat mats(arma::mat X, List model, int method, NumericVector h ) {
   
   arma::mat V = X.t() * X;
   arma::mat bread_1 = inv(V) * X.t(); 
   arma::mat bread_2 = X * V.i();
   
+  
 
+  
+  // criando o h usando quando method é 2, 3, 4 ou 5 no r
+  
+  // Quando method = 0
   if(method == 0){
     
     // criando a matriz error_hat
@@ -31,11 +36,35 @@ arma::mat mats(arma::mat X, List model, int method) {
     return hc;
   }
   
+  // Quando method == 2
+  if(method == 2){
+    // criando a matriz error_hat
+    NumericVector residuals =  model["residuals"];
+    int dim = residuals.size();
+    arma::mat error_hat = arma::zeros(dim,dim);
+    // ainda a questao da matriz error_hat
+    
+    // nao eh vetoriazado ou seja, passar um for aqui
+    NumericVector resi_2 = pow(residuals,2);
+    NumericVector h_1 = 1 - h;
+    
+    NumericVector vetor = resi_2/h_1;
+    
+    for (int i = 0; i < dim; i++){
+      error_hat(i,i) = vetor[i];
+    }
+    
+    arma::mat omega = error_hat;
+    arma::mat hc = bread_1 * omega * bread_2;
+    return hc;
+  }
+  
+  
   // return hc;
   
 }
 
 
  /*** R
-  mats(X = M, model = modelo, method = 0)
+  mats(X = M, model = modelo, method = 2,  h = h)
 */
