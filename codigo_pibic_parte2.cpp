b17f241f155a958d75f9eb1b18d20388299d4d30
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-02-15 23:56:02 -0300

    Com method = 0 funcionando correto

--- codigo_pibic_parte2.cpp
+++ codigo_pibic_parte2.cpp
@@ -8,22 +8,25 @@ using namespace Rcpp;
 arma::mat mats(arma::mat X, List model, int method) {
   
   arma::mat V = X.t() * X;
-  arma::mat bread1 = inv(V) * X.t(); 
-  arma::mat bread2 = X * V.i();
+  arma::mat bread_1 = inv(V) * X.t(); 
+  arma::mat bread_2 = X * V.i();
   
-  // criando a matriz error_hat
-  NumericVector residuals =  model["residuals"];
-  int dim = residuals.size();
-  arma::mat error_hat = arma::zeros(dim,dim);
-  
-  for (int i = 0; i < dim; i++){
-    error_hat(i,i) = residuals[i];
-  }
-  // ainda a questao da matriz error_hat
-  
+
   if(method == 0){
     
-    arma::mat hc = error_hat * 2;
+    // criando a matriz error_hat
+    NumericVector residuals =  model["residuals"];
+    int dim = residuals.size();
+    arma::mat error_hat = arma::zeros(dim,dim);
+    // ainda a questao da matriz error_hat
+    
+    // NumericVector residuals = pow(residuals,2);
+  
+    for (int i = 0; i < dim; i++){
+      error_hat(i,i) = residuals[i];
+    }
+    arma::mat omega = pow(error_hat,2);
+    arma::mat hc = bread_1 * omega * bread_2;
     
     return hc;
   }
