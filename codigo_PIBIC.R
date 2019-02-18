f0066a9eda162a2f24cf5da30238b799b8c3a405
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-02-17 23:11:08 -0300

    Função hc em r que puxa a funcao em C++.

--- codigo_PIBIC.R
+++ codigo_PIBIC.R
@@ -1,14 +1,5 @@
-Renda.Paraiba
-banco <- Renda.Paraiba
-attach(banco)
-
-modelo <- lm(RendaCapita ~  Adol2grau + AdultoSuperior + AdultosAnalfa + Morte1ano)
-summary(modelo)
-
-
-
-hcc <- function (model, method = 4, k = 0.7) 
-{
+hcc <- function (model, method = 4, k = 0.7){
+  
   if (class(model) != "lm") 
     stop("The argument model must have class lm.")
   if (method %in% c(0, 2, 3, 4, 5) == FALSE) {
@@ -18,28 +9,8 @@ hcc <- function (model, method = 4, k = 0.7)
   }
   
   M <- as.matrix(cbind(1, modelo$model[, -1]))
-  error_hat <- as.vector(modelo$residuals)
   h <- as.vector(hatvalues(modelo))
   
-  bread <- mats(M)
-  # bread_1 <- mats1(M)
-  # bread_2 <- mats2(M)
-
-  if (method == 0L) {
-    omega = diag(error_hat^2)
-    hc = bread_1 %*% omega %*% bread_2
-  }
-  if (method == 2L) omega = diag((error_hat^2)/(1 - h))
-  if (method == 3L) omega = diag((error_hat^2)/((1 - h)^2))
-  if (method == 4L) {
-    delta = pmin(4, h/mean(h))
-    omega = diag((error_hat^2)/((1 - h)^delta))
-    }
-  if (method == 5L) {
-    alpha = pmin(h/mean(h), max(4, k * max(h)/mean(h)))
-    omega = diag((error_hat^2)/sqrt((1 - h)^alpha))
-    }
-
-  hc(X = M, omega)
+  mats(X = M, model = modelo, method = method,  h = h, k = k)
 
 }
