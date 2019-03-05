e03623e754dfab5eef09e40a1bb4d7ea73938006
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-05 11:56:50 -0300

    Dificuldades com pow superadas.

--- funcao_pboot.cpp
+++ funcao_pboot.cpp
@@ -3,36 +3,30 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
-double pboot(List model, double significance) {
-  
-  
+
+float pboot(List model, double significance) {
   
   // no primeiro if
   NumericVector residuals =  model["residuals"];
-  NumericVector L = pow(residuals,3);
+  int length_res = residuals.size();
+  int L = pow(length_res,3);
   
   double sig_2 = significance * significance;
   double sig_plus = significance + 0.25;
-  double sig13;
-  double isso = 1/3;
-  for(int i = 0; i < 1; i++){
-    sig13 = pow(sig_plus,isso);
-  }
+  float sig13 = pow(sig_plus,1.0/3.0);
+  float gamma2 = 0.5 * (1/sig_2) * (1 - significance) * sig13;
   
-  // double sig_plus3 = pow(sig_plus,1/3);
-   double gamma2 = 0.5 * (1/sig_2) * (1 - significance) * (1/sig13);
+  int L2 = L*L;
+  float J = gamma2 * L2;
   
-  NumericVector L2 = L*L;
-  // NumericVector J = gamma2 * L2;
-  
-  NumericVector L3 = pow(L,3);
+  float L3 = pow(L,(2.0/3.0));
 
   //double gamma_1 = 1/gamma2;
-  // NumericVector K = (1/gamma2) * (1/L3);
+  float K = (1/gamma2) * L3;
   
   
   // fim do primeiro if.
-  return sig13;
+  return K;
 }
 
 
