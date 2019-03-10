35350c897e921e6d47632367f524dbe3c36b73e2
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-09 21:58:21 -0300

    Primeiro if em progresso, bool, int, NumericVector == NULL.

--- funcao_pboot.cpp
+++ funcao_pboot.cpp
@@ -1,35 +1,40 @@
 #include <RcppArmadillo.h>
+#include <cmath>
 using namespace Rcpp;
+
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
 
-float pboot(List model, double significance) {
+List pboot(List model, double significance, int J, int K){
   
   // no primeiro if
-  NumericVector residuals =  model["residuals"];
-  int length_res = residuals.size();
-  int L = pow(length_res,3);
+  if(J == 0 || K == 0){
+    NumericVector residuals =  model["residuals"];
+    int length_res = residuals.size();
+    int L =  length_res * length_res * length_res;
   
-  double sig_2 = significance * significance;
-  double sig_plus = significance + 0.25;
-  float sig13 = pow(sig_plus,1.0/3.0);
-  float gamma2 = 0.5 * (1/sig_2) * (1 - significance) * sig13;
+    double sig_2 = significance * significance;
+    double sig_plus = significance + 0.25;
+    float sig13 = pow(sig_plus,1.0/3.0);
+    float gamma2 = 0.5 * (1/sig_2) * (1 - significance) * sig13;
   
-  int L2 = L*L;
-  float J = gamma2 * L2;
-  
-  float L3 = pow(L,(2.0/3.0));
+    int L2 = L * L;
+    //
+    // TRANSFORMAR PARA INTEIRO
+    //
+    float JJ = gamma2 * L2;
+    int L3 = cbrt(L);
+    float KK = (1/gamma2) * L3;
 
-  //double gamma_1 = 1/gamma2;
-  float K = (1/gamma2) * L3;
-  
-  
-  // fim do primeiro if.
-  return K;
+
+    return List::create(_["J"] = JJ,
+                        _["K"] = KK);
+
+  }
 }
 
 
 /*** R
-pboot(modelo, 0.05)
+pboot(modelo, 0.01, J = 0, K = 0)
 */
