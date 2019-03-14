23ce29716f21e2f1903182a050195648fbec5235
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-14 12:34:50 -0300

    Add files via upload
    Utilizando is_wholenumber implementado para resultar um vetor resultou em erro fatal, como na tentativa de avaliar as posições do vetor e atribuir 1 quando TRUE e 0 quando false, mesmo tentando em outro pc.

--- is_wholenumber.cpp
+++ is_wholenumber.cpp
@@ -4,20 +4,22 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
-double is_wholenumber(NumericVector x, double tol){
+NumericVector is_wholenumber(NumericVector x, double tol){
   NumericVector rounding = round(x,0);
   NumericVector subtracao = x - rounding;
   NumericVector absol = abs(subtracao);
   
-  // para avaliar se eh menor tenho como fazer dessa forma
-  // int i = 1;
-  double avaliando = absol[1] - tol;
+  // arma::vec y = x;
   
+  NumericVector avaliando;
+  for(int i = 0; i < x.size(); i++){
+  avaliando[i] = absol[i] - tol;
+  }
   return avaliando;
 }
 
 /*** R
 set.seed(2019)
-s <- rnorm(1)
+s <- rnorm(20,100,100)
 is_wholenumber(s, .Machine$double.eps)
 */
