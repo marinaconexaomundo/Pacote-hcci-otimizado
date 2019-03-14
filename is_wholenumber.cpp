947ccb169a2af52e68406698ac3d2c033d4101ca
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-14 12:43:26 -0300

    Funcionando para avaliar um unico elemento.

--- is_wholenumber.cpp
+++ is_wholenumber.cpp
@@ -4,22 +4,12 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
-NumericVector is_wholenumber(NumericVector x, double tol){
+float is_wholenumber(NumericVector x, double tol){
   NumericVector rounding = round(x,0);
   NumericVector subtracao = x - rounding;
   NumericVector absol = abs(subtracao);
   
-  // arma::vec y = x;
+  float avaliando = absol[1] - tol;
   
-  NumericVector avaliando;
-  for(int i = 0; i < x.size(); i++){
-  avaliando[i] = absol[i] - tol;
-  }
   return avaliando;
 }
-
-/*** R
-set.seed(2019)
-s <- rnorm(20,100,100)
-is_wholenumber(s, .Machine$double.eps)
-*/
