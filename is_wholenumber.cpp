432adea02aadfea9e5c8e35bbce2ad96960fdc3a
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-13 23:21:48 -0300

    Saída como um booleano, porém o resultado da avaliação não está correta.

--- is_wholenumber.cpp
+++ is_wholenumber.cpp
@@ -5,18 +5,12 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 
 bool is_wholenumber(NumericVector x, double tol){
-  
-  bool boreliano;
   NumericVector rounding = round(x,0);
-  for(int i = 0; i < x.size(); i++){
-    
-   boreliano = wrap(abs(x[i] - rounding[i]) < tol);
-    
-  }
-  return boreliano;
+  return wrap(abs(x - rounding) < tol);
 }
 
 /*** R
-s <- rnorm(10)
+set.seed(2019)
+s <- rnorm(1)
 is_wholenumber(s, .Machine$double.eps)
 */
