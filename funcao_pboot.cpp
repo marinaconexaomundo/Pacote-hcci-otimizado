86dbb55d8c1f63c7d867ac24f08fc451672f7a6a
Marina Rodrigues <46030220+marinaconexaomundo@users.noreply.github.com>
2019-03-16 16:35:15 -0300

    Falta mudar como as variáveis whole's vai receber a saída da função is_wholenumber.

--- funcao_pboot.cpp
+++ funcao_pboot.cpp
@@ -5,15 +5,14 @@ using namespace Rcpp;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 
-List pboot(List model, double significance, int J, int K){
+NumericVector pboot(List model, double significance, NumericVector J, NumericVector K, double tol){
   
-  // no primeiro if
-  if(J == 0 || K == 0){
+  if(J[0] == 1 || K[0] == 1){
     
     NumericVector residuals =  model["residuals"];
     int length_res = residuals.size();
     
-    // trazendo do r a funcao "vector_escalar"
+    // trazendo do rcpp a funcao "vector_escalar"
     Function vector_escalar("vector_escalar");
     int L =  as<int>(vector_escalar(length_res, 2));
     
@@ -24,19 +23,39 @@ List pboot(List model, double significance, int J, int K){
     //
     // TRANSFORMAR PARA INTEIRO
     //
-    float JJ = gamma2 * L2;
+    J[1] = gamma2 * L2;
     int L3 = as<int>(vector_escalar(L, 1/3));
-    float KK = (1/gamma2) * L3;
+    K[1] = (1/gamma2) * L3;
     
-    // int int_J = as<int>JJ;
-    // int int_K = as<int>KK;
     
-    return List::create(_["J"] = JJ,
-                        _["K"] = KK);
+  // Trazendo do rcpp a funcao "is_wholenumber"
+  Function is_wholenumber("is_wholenumber");
+
+  NumericVector ava_J_significance = (J[1] + 1) * significance;
+  NumericVector J_K = (J[1] + 1)/K[1];
+  
+  double whole1 = is_wholenumber(K[1]/2, tol);
+  double whole2 = is_wholenumber(ava_J_significance, tol);
+  double whole3 = is_wholenumber(J_K, tol);
+  
+  while(whole1 > 0 &  whole2 > 0) {
+    
+    ava_J_significance = (J + 1) * significance;
+    J_K = (J + 1)/K;
+    
+    whole1 = is_wholenumber(K/2, tol);
+    whole2 = is_wholenumber(ava_J_significance, tol);
+    whole3 = is_wholenumber(J_K);
+    
+    while(whole1 > 0) K = K + 1;
+    while(whole2 > 0) J = J + 1;
+    while(whole3 > 0) K = K + 1;
   }
+  }
+}
 }
 
 
 /*** R
-pboot(modelo, 0.01, J = 0, K = 0)
+pboot(modelo, 0.01, J = NULL, K = NULL, tol = .Machine$double.eps ^ 0.5)
 */
